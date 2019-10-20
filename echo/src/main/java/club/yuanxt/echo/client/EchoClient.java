d2fe9d135b365379a6bd15c9a0be5b0765b603c8
layme <1149319668@qq.com>
2019-10-20 15:13:10 +0800

    echo

---
+++ echo/src/main/java/club/yuanxt/echo/client/EchoClient.java
@@ -0,0 +1,58 @@
+package club.yuanxt.echo.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.io.UnsupportedEncodingException;
+import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author
+ * @create 2019-09-27 21:09
+ * Description:
+ */
+public class EchoClient {
+    private final String host;
+    private final int port;
+
+    public EchoClient(String host, int port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public void start() throws InterruptedException {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                    .channel(NioSocketChannel.class)
+                    .remoteAddress(new InetSocketAddress(host, port))
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new EchoClientHandler());
+                        }
+                    });
+            ChannelFuture f = b.connect().sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException, UnsupportedEncodingException {
+//        new EchoClient("127.0.0.1", 60722).start();
+        String s = "i love you";
+        byte[] bytes = s.getBytes(StandardCharsets.UTF_8);
+        for (byte b : bytes) {
+            System.out.print(b);
+        }
+        System.out.println();
+    }
+}
